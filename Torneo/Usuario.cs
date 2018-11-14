af41057e70c75f0e9328df1e7056abbf9323fd3a
AlexanderGonzalezNiche <45024752+AlexanderGonzalezNiche@users.noreply.github.com>
2018-11-14 12:02:41 -0300

    ds

--- Torneo/Usuario.cs
+++ Torneo/Usuario.cs
@@ -18,7 +18,7 @@ namespace Torneo
 
         //constructor por defecto
         public Usuario() { }
-        //constructor Comun
+        //constructor Comunakakkaakaaka
         public Usuario(string nombre,string apellido,string nomusuario,string pass,string ciudad,List<Equipo> Equifavoritos,List<Partido> parevaluados)
         {
             Nombre = nombre;
