d92790ef175a63c0380deb0db8b7b550e93ec4df
AlexanderGonzalezNiche <45024752+AlexanderGonzalezNiche@users.noreply.github.com>
2018-11-14 10:45:42 -0300

    Entidades

---
+++ Torneo/Equipo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torneo
+{
+    class Equipo
+    {
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+        public string DT { get; set; }
+
+        public Equipo() { }
+        public Equipo(string nombre,string ciudad, string dt)
+        {
+            Nombre = nombre;
+            Ciudad = ciudad;
+            DT = dt;
+        }
+            
+
+
+    }
+}
