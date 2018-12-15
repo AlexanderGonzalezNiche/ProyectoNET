76a3694281072fcfce1365a4f03ff84a000cf976
AlexanderGonzalezNiche <45024752+AlexanderGonzalezNiche@users.noreply.github.com>
2018-12-15 17:58:56 -0300

    gridview

---
+++ WindowsFormsApp/IngresarResultado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Torneo_Clases;
+namespace WindowsFormsApp
+{
+    public partial class IngresarResultado : Form
+    {
+        private Fachada Fac;
+        public IngresarResultado()
+        {
+            InitializeComponent();
+        }
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+
+        }
+
+        private void IngresarResultado_Load(object sender, EventArgs e)
+        {
+            Fac = new Fachada();
+            dataGridView1.DataSource = Fac.DevolverPartidos();
+        }
+    }
+}
